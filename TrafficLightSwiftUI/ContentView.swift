c35a779fc6261282b770ffff4bab0b3c4d1eaa67
Alex <alexpesenka@gmail.com>
2025-02-10 20:15:17 +0100

    homework

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,16 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isFirstPress = true
+    @State private var currentLight = CurrentLight.red
+    
+    private let lightOn = 1.0
+    private let lightOff = 0.3
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            trafficLightCircle(color: .red, isOn: currentLight == .red )
+            trafficLightCircle(color: .yellow, isOn: currentLight == .yellow)
+            trafficLightCircle(color: .green, isOn: currentLight == .green)
+            Spacer()
+            
+            Button(action: changeTrafficLight) {
+                Text(isFirstPress ? "Start" : "Next")
+                    .padding(20)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
         }
         .padding()
+        
     }
 }
+
+private extension ContentView {
+    
+    func trafficLightCircle(color: Color, isOn: Bool) -> some View {
+        Circle()
+            .foregroundColor(color)
+            .opacity(isOn ? lightOn : lightOff)
+            .frame(width: 120, height: 120)
+    }
+    
+    func changeTrafficLight() {
+        if isFirstPress {
+            isFirstPress = false
+        }
+
+        switch currentLight {
+            case .red:
+            currentLight = .yellow
+        case .yellow:
+            currentLight = .green
+        case .green:
+            currentLight = .red
+        }
+    }
+    
+    enum CurrentLight {
+        case red, yellow, green
+    }
+    
+}
+
 
 #Preview {
     ContentView()
